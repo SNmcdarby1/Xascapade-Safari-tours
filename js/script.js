95211727adc692c49ba6a51977a8ea5cc5ad1df6
SNmcdarby1 <sngereso@gmail.com>
2021-01-07 17:47:18 +0000

    style edit

--- js/script.js
+++ js/script.js
@@ -9,20 +9,21 @@ $("").text(fontFamily);
 $("tr:odd").addClass("odd");
 $("tr:even").addClass("even");
 // 
-$("p").click(function() {
+$("p").click(function () {
     $("p").css("color", "aqua")
 });
-$("h").hover(function() {
+$("h").hover(function () {
     $("h3").css("background", "lightblue")
 });
-$(".card-panel").mouseenter(function() {
+$(".card-panel").mouseenter(function () {
     $("body").css("background-color", "black")
 });
-$(".card-panel").mouseleave(function() {
+$(".card-panel").mouseleave(function () {
     $("body").css("background-color", "rgba:(0, 0, 0, .3);");
 });
 // 
-
+// background img
+$('myObject').css('background-image', 'url(' + imageUrl + ')');
 
 -
 // 
@@ -35,155 +36,75 @@ $("div").click()
     (".module-1")
     (".module-2").empty();
 // 
-$("p").click(function() {
+$("p").click(function () {
     $("p").text("All Changed");
 });
 // 
-$("").click(function() {
+$("").click(function () {
 
 });
 
 
-$("image").click(function() {})
 // 
-{
-    $(document).ready(function() {
-        $("p").click(function() {
-            $(this).text("Changed only the text for this paragraph")
-        });
-
-        $(".card").hover(function() {
-            $(".card").mouseenter(function() {
-                $(this).fadeTo(2000, 0.2).fadeTo(2000, 0.2)
-                $(this).fadeOut(2000, 1).fadeOut(2000, 1)
-            });
-        });
-    });
-    // 
-    $('').css(function(_newFunction(''));
-
-
-
-        // 
-        $function(e) {
-            else("submit")
-            if (condition)
-                console.error(); {
-
-            }
-            enter {
-
-            }("404")
-        });
-    // 
-    client.on('message', message => {
-        //console.log(message.content);
-
-        if (message.content.startsWith(`${prefix}`)) {
-            message.channel.send("oh yeah it's the prefix of the bot")
-        }
-
-        if (message.content.startsWith(`${prefix}gif`)) {
-            giphy.trending("gifs", {})
-                .then((response) => {
-                    var totalResponses = response.data.length;
-                    var responseIndex = Math.floor((Math.random() * 10) + 1) % totalResponses;
-                    var responseFinal = response.data[responseIndex]
-
-                    message.channel.send("There you go!", {
-                        files: [responseFinal.images.fixed_height.url]
-                    })
-                    file
-                })
-        }
+$(".button").hover(function () {
+    $("button").mouseenter(function () {
+        $("button").removeClass("make-red").addClass("make-border");
     })
-    // 
-    $(".button").hover(function() {
-        $("button").mouseenter(function() {
-            $("button").removeClass("make-red").addClass("make-border");
-        })
+})
+$(".button").hover(function () {
+    $("button").mouseleave(function () {
+        $("button").removeClass("make-border").addClass("make-red");
     })
-    $(".button").hover(function() {
-        $("button").mouseleave(function() {
-            $("button").removeClass("make-border").addClass("make-red");
-        })
-    })
-    // 
-    $("#myButton").removeClass("border").addClass("blueBox");
+})
+// 
+$("#myButton").removeClass("border").addClass("blueBox");
 
-    // 
+// 
 
-    $("#button-card-1").click(function() {
-        $("#par-1").hide(2000).show(2000);
-    });
-    $("#button-card-2").click(function() {
-        $("#par-2").fadeOut(1000).fadeIn(1000);
-    });
-
-    .column {
-        right;
-
-        10 px;
-    }
-
-    /* Clear floats after the columns */
+$("#button-card-1").click(function () {
+    $("#par-1").hide(2000).show(2000);
+});
+$("#button-card-2").click(function () {
+    $("#par-2").fadeOut(1000).fadeIn(1000);
+});
 
 
 
-    // 
+// 
 
 
-    // 
-    $('module-nav').on("click", function() {
-        var allModulesCardsSelector = ".card";
-        var thisModuleCardsSelector = "." + this.id + "-card";
-        $(allModulesCardsSelector).removeClass("card-hihglight")
-        $(thisModuleCardsSelector).addClass("card-hihglight")
-    });
+// 
 
-
-    $('module-nav').on("click", function() {
-        let allModulesCardsSelector = ".card";
-        let thisModuleCardsSelector = "." + this.id + "-card";
-        $(allModulesCardsSelector).removeClass("card-highlight");
-        $(thisModuleCardsSelector).addClass("card-highlight");
-    });
-
-
-    $("p").click(function() {
-        $(this).children("a").css("background-color", "yellow")
-    });
-
-    // 
-    var arr1 = ['Sarova Hotels', 'Serena hotels', 'Intercontinental Hotels', 'Camping', 'AirBnB', 'Safari Park Hotel Nairobi', 'Diani Hotel', 'White Sands Holtel',
-        'Nakuru Lodge Hotel',
-        'Intercontinental Hotel Nairobi',
-        'Stanlley Hotel',
-        'Red Elephant Hotel Tsavo',
-        'Reef Hotel Mombasa',
-        'Nyali Hotel Mombasa’',
-        'Voyager Hotel Mombasa', 'African Safari Club',
-        'La Pelgola Malindi',
-        'Mada Hotel Kilifi',
-        'Bamburi Beach Resort Mombasa',
-        'Kilifi Bay Resort Kilifi',
-        'Sun and Sand Club Mombasa',
-        'Turtle Bay Watamu ',
-        'Windsor Hotel',
-        'FairMount Hotel and Resort Nairobi'
-    ];
-    var arr2 = ['safari', 'fish', 'apple', 'pizza'];
-    'Theme',
-    'parks',
-    'Snorkkling',
-    'tour',
-    'City tours',
-    'Museum',
-    'UNESCO Sights',
-    'Masaai Market',
-    'Open village market',
-    'City mall',
-    'Cinema'];
+// 
+var arr1 = ['Sarova Hotels', 'Serena hotels', 'Intercontinental Hotels', 'Camping', 'AirBnB', 'Safari Park Hotel Nairobi', 'Diani Hotel', 'White Sands Holtel',
+    'Nakuru Lodge Hotel',
+    'Intercontinental Hotel Nairobi',
+    'Stanlley Hotel',
+    'Red Elephant Hotel Tsavo',
+    'Reef Hotel Mombasa',
+    'Nyali Hotel Mombasa’',
+    'Voyager Hotel Mombasa', 'African Safari Club',
+    'La Pelgola Malindi',
+    'Mada Hotel Kilifi',
+    'Bamburi Beach Resort Mombasa',
+    'Kilifi Bay Resort Kilifi',
+    'Sun and Sand Club Mombasa',
+    'Turtle Bay Watamu ',
+    'Windsor Hotel',
+    'FairMount Hotel and Resort Nairobi'
+];
+var arr2 = ['safari', 'fish', 'apple', 'pizza'];
+'Theme',
+'parks',
+'Snorkkling',
+'tour',
+'City tours',
+'Museum',
+'UNESCO Sights',
+'Masaai Market',
+'Open village market',
+'City mall',
+'Cinema'];
 
 
 // Theme parks Snorkkling tour City tours Museum UNESCOSights Masaai Market Open village market City mall Cinema
@@ -207,17 +128,24 @@ var arr3 = ['Taxi', 'Private Jeep', 'Saloon car', 'Private car', 'Safari Van', '
 
 
 
-var result = arrays.shift().filter(function(v) {
-    return arrays.every(function(a) {
+var result = arrays.shift().filter(function (v) {
+    return arrays.every(function (a) {
         return a.indexOf(v) !== -1;
     });
 });
 
-arrays.sort(function(a, b) {
+arrays.sort(function (a, b) {
     return a.length - b.length;
 });
 
 function newFunction() {
     return '';
 }
+// $npm install @fortawesome/fontawesome-svg-core
 // 
+l.GroupBy(x => x.id).
+Select(x => {
+    var ret = x.First();
+    ret.value = x.Sum(xt => xt.value);
+    return ret;
+}).ToList();
