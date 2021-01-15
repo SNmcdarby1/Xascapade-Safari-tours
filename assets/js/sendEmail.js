a36cb4473541dfb714e16d2a4db12ce9cbf94e3c
SNmcdarby1 <sngereso@gmail.com>
2021-01-15 07:12:10 +0000

    debugging js

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,19 +1,17 @@
-function() {
-    emailjs.init("user_4yI8AhyqyWQa3VahbN5Ra")
-};
-
-function sendMail(contactForm) {
-    emailjs.send("gmail", "rosie", {
-            "from_name": contactForm.name.value,
-            "from_email": contactForm.emailaddress.value,
-            "project_request": contactForm.projectsummary.value
-        })
-        .then(
-            function(response) {
-                console.log("SUCCESS", response);
+function sendEmail(params) {
+    var tempParams = {
+        from_name: document.getElementById("fromName").value,
+        to_name: document.getElementById("toName").value,
+        message: document.getElementById("message").value,
+    };
+    emailjs.send('service_y2i261b', 'template_gjh9ejg',
+            tempParam)
+        .then(function (res) {
+                console.log("success", res.status);
             },
-            function(error) {
-                console.log("FAILED", error);
-            }
-        );
-    return false; // To block from loading a new page
+            ifelse(function (error) {
+                console.log("failed", error);
+            });
+            return false; // To block from loading a new page
+            // 
+        });
