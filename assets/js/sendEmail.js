85fd9a53a65bf4c24f3df68410b3685b896d1e2e
SNmcdarby1 <sngereso@gmail.com>
2021-01-15 09:21:50 +0000

    modification sendemail.js

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,15 +1,15 @@
 function sendEmail(params) {
     var tempParams = {
         from_name: document.getElementById("fromName").value,
-        to_name: document.getElementById("toName").value,
+        to_name: document.getElementById("toname").value,
         message: document.getElementById("message").value,
     };
-    emailjs.send('service_y2i261b', 'template_gjh9ejg',
+    emailjs.send("service_y2i261b", "template_gjh9ejg",
             tempParam)
-        .then(function (res) {
+        .then(function(res) {
                 console.log("success", res.status);
             },
-            ifelse(function (error) {
+            ifelse(function(error) {
                 console.log("failed", error);
             });
             return false; // To block from loading a new page
