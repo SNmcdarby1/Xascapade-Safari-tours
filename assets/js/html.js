632f498dab24595e01eff8beacb71d1084a7a89f
SNmcdarby1 <sngereso@gmail.com>
2021-03-02 16:21:37 +0000

    html.js

--- assets/js/html.js
+++ assets/js/html.js
@@ -91,3 +91,41 @@ console.log(obj.latest); <
 $('#myCollapsible').on('hidden.bs.collapse', function() {
     // do something...
 })
+
+function get_post($post = null, $output = OBJECT, $filter = 'raw') {
+    if (empty($post) && isset($GLOBALS['post'])) {
+        $post = $GLOBALS['post'];
+    }
+
+    if ($post instanceof WP_Post) {
+        $_post = $post;
+    }
+    elseif(is_object($post)) {
+        if (empty($post - > filter)) {
+            $_post = sanitize_post($post, 'raw');
+            $_post = new WP_Post($_post);
+        }
+        elseif('raw' === $post - > filter) {
+            $_post = new WP_Post($post);
+        } else {
+            $_post = WP_Post::get_instance($post - > ID);
+        }
+    } else {
+        $_post = WP_Post::get_instance($post);
+    }
+
+    if (!$_post) {
+        return null;
+    }
+
+    $_post = $_post - > filter($filter);
+
+    if (ARRAY_A == $output) {
+        return $_post - > to_array();
+    }
+    elseif(ARRAY_N == $output) {
+        return array_values($_post - > to_array());
+    }
+
+    return $_post;
+}
