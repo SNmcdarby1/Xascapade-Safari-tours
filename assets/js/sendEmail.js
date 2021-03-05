8e1ece39d6573f4ebc2ef2367781fc15fe8791ea
SNmcdarby1 <sngereso@gmail.com>
2021-03-05 13:21:09 +0000

    edit sendEmail.js

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -15,7 +15,8 @@ function sendEmail(tempParams) {
     return false;
 }
 // 
-if (empty($_POST['name']) ||
+function
+email (_email($_POST['name']) ||
     empty($_POST['email']) ||
     empty($_POST['phone']) ||
     empty($_POST['message']) ||
@@ -28,7 +29,7 @@ $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
-// Create the email and send the message
+// Create th=e email and send the message
 $to = 'xascapade.com'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n".
